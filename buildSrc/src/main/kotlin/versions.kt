1e8563c193bd013e958353328db7790e91f773c0
andylamax <andylamax@programmer.net>
2021-01-10 18:51:52 +0300

    Updated version

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -6,7 +6,7 @@ object vers {
     }
 
     object asoft {
-        val access = "0.0.11"
+        val access = "0.0.12"
         val contacts = "0.0.1"
         val persist = "0.1.10"
         val builders = "1.3.0"

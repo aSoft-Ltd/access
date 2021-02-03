24ebcbaa30798bf7b36a96940227213a03c18df8
andylamax <andylamax@programmer.net>
2021-02-03 12:29:19 +0300

    bumped version to 0.0.20

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -7,7 +7,7 @@ object vers {
     }
 
     object asoft {
-        val access = "0.0.14"
+        val access = "0.0.20"
         val contacts = "0.0.1"
         val persist = "0.2.10"
         val builders = "1.3.0"
